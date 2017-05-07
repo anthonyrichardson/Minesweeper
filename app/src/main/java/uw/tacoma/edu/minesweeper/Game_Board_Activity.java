358e6c16fa78475ac6435afc59015ab61527db8d
SDanh <sopheaktra.danh@gmail.com>
2017-05-07 16:54:35 -0700

    Changed from Grid Layout to Grid View, because Grid View scrolls.

--- app/src/main/java/uw/tacoma/edu/minesweeper/Game_Board_Activity.java
+++ app/src/main/java/uw/tacoma/edu/minesweeper/Game_Board_Activity.java
@@ -2,12 +2,26 @@ package uw.tacoma.edu.minesweeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class Game_Board_Activity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game__board_);
+
+        GridView gridview = (GridView) findViewById(R.id.game_board);
+        //gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(), position, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
