4bb3f89b546666887e8873d265d66bd520beec33
tony48 <tony48@uw.edu>
2017-05-07 21:32:04 -0700

    added buttons and a title to activity_welcome.xml created dimens.xml resource file to sore dimensions for elements and text

---
+++ app/src/main/java/uw/tacoma/edu/minesweeper/WelcomeActivity.java
@@ -0,0 +1,32 @@
+
+
+package uw.tacoma.edu.minesweeper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * The first screen for the game.
+ *
+ * @author Tony Richardson
+ */
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    /**
+     * Starts the minesweeper game board activity.
+     *
+     * @param view
+     */
+    public void gotoMinesweeper(View view) {
+        Intent intent = new Intent(this Minesweeper.class);
+        startActivity(intent);
+    }
+}
