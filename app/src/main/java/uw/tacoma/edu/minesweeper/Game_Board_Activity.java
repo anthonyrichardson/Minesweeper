6d75cbb9d2feb64fffa4d3839b05cd195403b92e
SDanh <sopheaktra.danh@gmail.com>
2017-05-07 20:46:26 -0700

    Created Clickable Cells for Grid and Temporary Button to go from Main Activity to Game Board Activity.

--- app/src/main/java/uw/tacoma/edu/minesweeper/Game_Board_Activity.java
+++ app/src/main/java/uw/tacoma/edu/minesweeper/Game_Board_Activity.java
@@ -4,24 +4,42 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Game_Board_Activity extends AppCompatActivity {
 
+    GridView gridView;
+
+    static final String[] numbers = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_game__board_);
 
-        GridView gridview = (GridView) findViewById(R.id.game_board);
-        //gridview.setAdapter(new ImageAdapter(this));
+        gridView = (GridView) findViewById(R.id.game_board);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, numbers);
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v,
                                     int position, long id) {
-                Toast.makeText(getApplicationContext(), position, Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
             }
         });
+
     }
 }
