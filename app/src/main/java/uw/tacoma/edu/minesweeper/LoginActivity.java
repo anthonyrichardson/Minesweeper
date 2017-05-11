cedc657207186d3d2120ba48cd1e5747e8363d6f
tony48 <tony48@uw.edu>
2017-05-11 15:59:48 -0700

    Commit Branch Danh from tony

--- app/src/main/java/uw/tacoma/edu/minesweeper/LoginActivity.java
+++ app/src/main/java/uw/tacoma/edu/minesweeper/LoginActivity.java
@@ -3,6 +3,7 @@ package uw.tacoma.edu.minesweeper;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,7 +18,7 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class LoginActivity extends AppCompatActivity implements addAccountFragment.addAccountListener {
+public class LoginActivity extends AppCompatActivity implements addAccountFragment.addAccountListener, LoginFragment.loginListener {
 
 
 
@@ -37,10 +38,32 @@ public class LoginActivity extends AppCompatActivity implements addAccountFragme
                         .commit();
             }
         });
+
+
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginFragment loginFragment = new LoginFragment();
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, loginFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+
+
+
+
+
     }
 
-    public void login_account(String url){
+    public void login(String url) {
+        //Toast.makeText(getApplicationContext(), url, Toast.LENGTH_LONG).show();
 
+        LoginTask loginTask = new LoginTask();
+        loginTask.execute(new String[]{url.toString()});
+        getSupportFragmentManager().popBackStackImmediate();
     }
 
     public void addAccount(String url){
@@ -51,6 +74,90 @@ public class LoginActivity extends AppCompatActivity implements addAccountFragme
         addAccountTask.execute(new String[]{url.toString()});
         getSupportFragmentManager().popBackStackImmediate();
     }
+
+
+
+
+
+
+
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to login, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+            // Something wrong with the network or the URL.
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String status = (String) jsonObject.get("result");
+                if (status.equals("success")) {
+                    Toast.makeText(getApplicationContext(), "Login successful!"
+                            , Toast.LENGTH_LONG)
+                            .show();
+                    //Log.i()
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login failed: "
+                                    + jsonObject.get("error")
+                            , Toast.LENGTH_LONG)
+                            .show();
+
+                }
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data\n" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+                Log.d("Login task","Something wrong with the data", e);
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+
 
     private class AddAccountTask extends AsyncTask<String, Void, String> {
         @Override
